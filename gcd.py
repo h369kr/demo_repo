160534ed2e5246bbaaf210bc16aeacd0c778b2e9
Harsh Kumar <h369kr@gmail.com>
2022-12-20 16:27:20 +0530

    gcd-code-push

---
+++ gcd.py
@@ -0,0 +1,30 @@
+a = int(input())
+b = int(input())
+
+list1 = []
+def fact1(x):
+    for i in range (1,int(a/2)+1):
+        if a % i == 0:
+            list1.append(i)
+
+list2 = []
+def fact2(y):
+    for i in range (1,int(b/2)+1):
+        if b % i == 0:
+            list2.append(i)
+
+fact1(a)
+fact2(b)
+
+if a>b:
+    for i in reversed(list1):
+        for j in reversed(list2):
+            if i == j:
+                print(i)
+            break
+else:
+    for i in reversed(list2):
+        for j in reversed(list1):
+            if i == j:
+                print(i)
+            break
